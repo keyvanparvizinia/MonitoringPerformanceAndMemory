3396d077f0a5dcc1c1354b1b7af7eb44ab6b2147
K1 <K1@DESKTOP-VJ74ISC>
2023-05-05 12:29:21 +0430

    Measuring the efficiency of processing strings

---
+++ ProcessingStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using MonitoringUsingDiagnostics;
+using static System.Console;
+
+int[] numbers = Enumerable.Range(
+    start: 1, count: 50_000).ToArray();
+
+WriteLine("Using string with +");
+Recorder.Start();
+string s = string.Empty; // i.e. ""
+for (int i = 0; i < numbers.Length; i++)
+{
+    s += numbers[i] + ", ";
+}
+Recorder.Stop();
+
+WriteLine("************************************");
+
+WriteLine("Using StringBuilder");
+Recorder.Start();
+System.Text.StringBuilder builder = new();
+for (int i = 0; i < numbers.Length; i++)
+{
+    builder.Append(numbers[i]);
+    builder.Append(", ");
+}
+Recorder.Stop();
